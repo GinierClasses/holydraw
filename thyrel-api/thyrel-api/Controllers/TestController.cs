79b36370c4a13f3e72d987ec291245213a392f7f
Melvynx <melvyn.malherbe@qoqa.com>
2021-01-25 19:29:10 +0100

    add base app + test controller

---
+++ thyrel-api/thyrel-api/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace thyrel_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/Test
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            yield return "API is happy to see you";
+        }
+
+        // POST: api/Test
+        [HttpPost]
+        public IEnumerable<string> Post([FromBody] string value)
+        {
+            return new String[] { "Here is you're value", value };
+        }
+    }
+}
