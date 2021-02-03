64c51a951d01eab9b14aa4dee54ea3f3d7ea95ad
Melvynx <melvyn.malherbe@qoqa.com>
2021-02-03 09:06:42 +0100

    remove: useless console.log

--- thyrel-web/src/__tests__/App.test.tsx
+++ thyrel-web/src/__tests__/App.test.tsx
@@ -3,6 +3,5 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 test('renders learn react link', () => {
-  console.log(process.env.REACT_APP_API_URL);
   render(<App />);
 });
