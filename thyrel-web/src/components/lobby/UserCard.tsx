2f761c4dac5bcff9c8be95b01338947d8fcc5cde
Alexandre Javet <alexandre.javet@hotmail.com>
2021-02-04 15:30:50 +0100

    [add] UserCard Component
    - Can set following parameters:
    	- id `number`
    	- name `string`
    	- avatar `string`
    	- isOwner `boolean`
    	- isKickable `boolean`
    	- onKick `function, takes id (number) as parameter`
    - Name shrinks in size if too big and trunkates if it's still too big
    - `avatar` props sets the avatar picture
    - `isOwner` will show a star on the user card if true
    - `isKickable` will show a cross (button that executes `onKick function`) if true and if isOwner is not true

---
+++ thyrel-web/src/components/lobby/UserCard.tsx
@@ -0,0 +1,77 @@
+// src/components/UserCard.tsx
+
+// Imports of components
+import { Avatar } from 'rsuite';
+import { css } from '@emotion/css';
+import styled from '@emotion/styled';
+import Box from '../../styles/Box';
+import { Icon } from 'rsuite';
+
+type UserCardProps = {
+  id: number;
+  name: string;
+  avatar: string;
+  isOwner: boolean;
+  isKickable: boolean;
+  onKick?: (id: number) => void;
+};
+
+// Sets size for avatar component
+const StyledAvatar = styled(Avatar)({
+  width: '48px',
+  height: '48px',
+});
+
+// The component itself
+export default function UserCard({
+  id,
+  name,
+  avatar,
+  isOwner,
+  isKickable,
+  onKick,
+}: UserCardProps) {
+  return (
+    <Box
+      borderWidth={1}
+      width={192}
+      height={64}
+      borderRadius={4}
+      padding={8}
+      alignItems="center"
+      justifyContent="space-between"
+      className={css({
+        background: '#0F131A',
+      })}>
+      <StyledAvatar circle={true} src={avatar} size="lg" />
+      <p
+        className={css({
+          fontFamily: 'Work Sans',
+          fontSize: 24 + 2 - name.length,
+          fontWeight: 'bold',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          textOverflow: 'ellipsis',
+          maxWidth: 80,
+        })}>
+        {name}
+      </p>
+      <div>
+        {isOwner ? (
+          <Icon icon="twinkle-star" />
+        ) : (
+          isKickable && (
+            <button
+              onClick={() => onKick?.(id)}
+              className={css({
+                backgroundColor: 'transparent',
+                outline: 'none',
+              })}>
+              <Icon icon="close" />
+            </button>
+          )
+        )}
+      </div>
+    </Box>
+  );
+}
--- thyrel-web/src/pages/Lobby.tsx
+++ thyrel-web/src/pages/Lobby.tsx
@@ -1,4 +1,16 @@
+import UserCard from '../components/lobby/UserCard';
+
 // component when we are in a Lobby (in waiting of game start)
 export default function Lobby() {
-  return <div>You're in Lobby</div>;
+  return (
+    // This is the test for the UserCard component
+    <UserCard
+      id={1}
+      name="Jean"
+      avatar="https://cutt.ly/Bkj9im1"
+      isOwner={false}
+      isKickable={true}
+      onKick={id => console.log("Here's the id:", id)}
+    />
+  );
 }
