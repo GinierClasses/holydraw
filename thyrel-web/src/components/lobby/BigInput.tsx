bcc99a6dcd1095f78fa8f9bdb99a8556c5f82138
Melvynx <melvyn.malherbe@qoqa.com>
2021-02-09 20:09:02 +0100

    changes

--- thyrel-web/src/components/lobby/BigInput.tsx
+++ thyrel-web/src/components/lobby/BigInput.tsx
@@ -1,37 +1,44 @@
+import React from 'react';
 import { css } from '@emotion/css';
 import styled from '@emotion/styled';
-import { Icon, IconProps, Input, InputGroup, InputProps } from 'rsuite';
-import { baseColor, bgColor, paperColor } from '../../styles/colors';
+import { IconProps } from 'rsuite';
+import { baseColor, bgColor } from '../../styles/colors';
 
-type BigInputProps = InputProps & {
+type BigInputProps = {
   icon?: IconProps['icon'];
   placeholder?: string;
 };
 
-const Ig = styled(InputGroup)({
-  height: 48,
-  borderWidth: 4,
-  borderColor: baseColor,
+const Container = styled.div<{ focus: boolean }>(
+  {
+    display: 'flex',
+    backgroundColor: bgColor,
+    border: `1px solid ${baseColor}`,
+    height: 48,
+    borderRadius: 4,
+    padding: 8,
+    '&:focus': {
+      background: 'red',
+    },
+  },
+  ({ focus }) => focus && {},
+);
+
+const Input = styled.input({
+  display: 'flex',
+  backgroundColor: 'inherit',
+  border: 'none',
+  width: '100%',
+  fontSize: 32,
+  fontFamily: 'Work Sans',
 });
 
 export default function BigInput({ icon, ...props }: BigInputProps) {
+  const [focus, setFocus] = React.useState(false);
+
   return (
-    <InputGroup className="rsuit-input-group__override">
-      {icon && (
-        <InputGroup.Addon
-          className={css({
-            backgroundColor: paperColor,
-          })}>
-          <Icon icon={icon} size="2x" />
-        </InputGroup.Addon>
-      )}
-      <Input
-        className={css({
-          padding: icon ? '8px 4px' : undefined,
-        })}
-        {...props}
-        size="lg"
-      />
-    </InputGroup>
+    <Container focus={focus}>
+      <Input onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} />
+    </Container>
   );
 }
