6714ba5ace46f20c4f1200bf017130ccdbcdbeea
TacticsCH <alexandre.javet@hotmail.com>
2021-02-13 15:04:53 +0100

    add: Start function in SessionController

--- thyrel-api/thyrel-api/Controllers/SessionController.cs
+++ thyrel-api/thyrel-api/Controllers/SessionController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using thyrel_api.Controllers.ModelsControllers;
 using thyrel_api.Models;
 using thyrel_api.Websocket;
 
@@ -17,6 +19,23 @@ namespace thyrel_api.Controllers
             _websocketHandler = websocketHandler;
         }
 
-       
+        public class StartBody
+        {
+            public int RoomId;
+        }
+
+        // Call this endpoint to create a room
+        // POST: api/room
+        [HttpPost]
+        public ActionResult<Player> Start([FromBody] StartBody body)
+        {
+            var sessionDataProvider = new SessionDataProvider();
+            var playerDataProvider = new PlayerDataProvider();
+
+            sessionDataProvider.Add(body.RoomId);
+
+            playerDataProvider.GetPlayersByRoom(body.RoomId).ForEach(p => );
+
+        }
     }
 }
