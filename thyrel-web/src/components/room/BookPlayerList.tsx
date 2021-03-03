0af487b0b703a19af9d8ca005c15dfd085fa4b1c
TacticsCH <alexandre.javet@hotmail.com>
2021-03-03 08:56:31 +0100

    feat: BookPlayerList component
    - Takes a list of players
    - Highlights selected player (playerId)
    - Tooltip on avatar shows username
    
    add: overflowX in Box component

--- thyrel-web/src/components/room/BookPlayerList.tsx
+++ thyrel-web/src/components/room/BookPlayerList.tsx
@@ -1,59 +1,63 @@
 import Box from '../../styles/Box';
-import BookPlayerList from './BookPlayerList';
 import Player from '../../types/Player.type';
 import { Avatar } from 'rsuite';
-import { baseColor } from '../../styles/colors';
-import styled from '@emotion/styled/types/base';
+import { primaryFade } from '../../styles/colors';
+import styled from '@emotion/styled';
+import { Tooltip, Whisper } from 'rsuite';
+import { css } from '@emotion/css';
 
-type BookPlayerListListProps = {
+type BookPlayerListProps = {
   players: Player[];
-  isKickable: boolean;
-  onKick?: (id: number) => void;
+  playerId?: number;
 };
 
 const StyledAvatar = styled(Avatar)({
-  width: 48,
-  height: 48,
-  backgroundColor: 'transparent',
-  border: `1px solid ${baseColor}`,
+  width: 32,
+  height: 32,
   overflow: 'visible',
   '> img': {
     // `!important` is awful in CSS but I don't have the choice
-    height: '48px !important',
+    height: '26px !important',
     width: 'auto !important',
     margin: 'auto',
   },
 });
 
-export default function BookPlayerListList({
+export default function BookPlayerList({
   players,
-  isKickable,
-  onKick,
-}: BookPlayerListListProps) {
+  playerId,
+}: BookPlayerListProps) {
   return (
     <Box
-      gap={16}
-      height={270}
-      width={150}
-      overflowY="scroll"
+      gap={8}
+      margin="0px 0px 25px 0px"
+      height={50}
+      width={200}
+      overflowX="scroll"
       alignItems="center"
-      flexDirection="row"
-      pr={8}>
+      flexDirection="row">
       {players.map(player => {
+        const isPlayerSelected = playerId === player.id;
         return (
-          
+          <div>
+            <Whisper
+              placement="top"
+              trigger="hover"
+              speaker={<Tooltip>{player.username}</Tooltip>}>
+              <StyledAvatar
+                className={css({
+                  backgroundColor: isPlayerSelected
+                    ? primaryFade(0.6)
+                    : primaryFade(0.2),
+                })}
+                circle={true}
+                src={player.avatarUrl}
+                size="lg"
+              />
+            </Whisper>
+          </div>
         );
       })}
     </Box>
   );
 }
-
-{/* <BookPlayerList
-id={player.id}
-name={player.username}
-avatar={player.avatarUrl}
-isOwner={player.isOwner}
-isKickable={isKickable}
-onKick={onKick}
-key={player.id}
-/> */}
--- thyrel-web/src/pages/ComponentTest.tsx
+++ thyrel-web/src/pages/ComponentTest.tsx
@@ -11,6 +11,7 @@ import profilesPictures from '../images/profiles/profiles-pictures';
 import Box from '../styles/Box';
 import DirectiveLabel from '../components/room/DirectiveLabel';
 import SizePicker from '../components/room/SizePicker';
+import BookPlayerList from '../components/room/BookPlayerList';
 
 export default function ComponentTest() {
   const [ppIndex, setPpIndex] = useState(0);
@@ -27,8 +28,8 @@ export default function ComponentTest() {
 
         <Box display="block" width={100} height={100}>
           <StepTimer
-            finishAt={new Date('2021-02-10T15:15:00')}
-            timeDuration={1000}
+            finishAt={new Date('2021-03-02T10:27:00')}
+            timeDuration={100}
             onFinish={() => console.log('finished')}
           />
         </Box>
@@ -133,6 +134,74 @@ export default function ComponentTest() {
             setSize(size);
           }}
         />
+
+        <BookPlayerList
+          players={[
+            {
+              id: 1,
+              username: 'jeanmich',
+              avatarUrl: profilesPictures[0],
+              isOwner: true,
+              isPlaying: true,
+              createdAt: '',
+              roomId: 1,
+            },
+            {
+              id: 2,
+              username: 'Xx_plao',
+              avatarUrl: profilesPictures[1],
+              isOwner: false,
+              isPlaying: true,
+              createdAt: '',
+              roomId: 1,
+            },
+            {
+              id: 3,
+              username: 'AAAAAAHHHH',
+              avatarUrl: profilesPictures[2],
+              isOwner: false,
+              isPlaying: true,
+              createdAt: '',
+              roomId: 1,
+            },
+            {
+              id: 4,
+              username: 'Melvyn',
+              avatarUrl: profilesPictures[3],
+              isOwner: false,
+              isPlaying: true,
+              createdAt: '',
+              roomId: 1,
+            },
+            {
+              id: 5,
+              username: 'Ana',
+              avatarUrl: profilesPictures[4],
+              isOwner: false,
+              isPlaying: true,
+              createdAt: '',
+              roomId: 1,
+            },
+            {
+              id: 6,
+              username: 'Alex',
+              avatarUrl: profilesPictures[5],
+              isOwner: false,
+              isPlaying: true,
+              createdAt: '',
+              roomId: 1,
+            },
+            {
+              id: 7,
+              username: 'Luca',
+              avatarUrl: profilesPictures[6],
+              isOwner: false,
+              isPlaying: true,
+              createdAt: '',
+              roomId: 1,
+            },
+          ]}
+          playerId={3}></BookPlayerList>
       </Box>
     </Box>
   );
