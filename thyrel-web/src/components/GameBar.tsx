e6b4836fcd24fff16797cd58f0deb73e37a6d7cc
TacticsCH <alexandre.javet@hotmail.com>
2021-03-03 11:36:24 +0100

    feat: Start react view is now responsive
    - Modified GameBar
    	- Component now "*folds*" when on mobile devices
    	- Timer component modified
    		- Timer shrinks in size
    - Modified Start react view (BigInput has now a placeholder instead of a value, my bad)

--- thyrel-web/src/components/GameBar.tsx
+++ thyrel-web/src/components/GameBar.tsx
@@ -2,6 +2,8 @@ import PlayerCount from './room/PlayerCount';
 import AppTitle from './lobby/AppTitle';
 import StepTimer from './room/StepTimer';
 import Box from '../styles/Box';
+import { css } from '@emotion/css';
+import Mq from '../styles/breakpoint';
 
 type GameBarProps = {
   count: number;
@@ -19,17 +21,42 @@ export default function GameBar({
   onFinish,
 }: GameBarProps) {
   return (
-    <Box flexDirection="row" width="100%">
-      <PlayerCount count={count} max={max} />
-
+    <Box
+      className={css({
+        flexDirection: 'column',
+        alignItems: 'center',
+      })}
+      width="100%">
       <AppTitle />
 
-      <Box display="block" width={100} height={100}>
-        <StepTimer
-          finishAt={finishAt}
-          timeDuration={timeDuration}
-          onFinish={onFinish}
-        />
+      <Box
+        className={css({
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          width: '100%',
+          [Mq.SM]: {
+            position: 'relative',
+            top: '-64px',
+          },
+        })}>
+        <PlayerCount count={count} max={max} />
+
+        <Box
+          display="block"
+          className={css({
+            height: 64,
+            width: 64,
+            [Mq.SM]: {
+              height: 100,
+              width: 100,
+            },
+          })}>
+          <StepTimer
+            finishAt={finishAt}
+            timeDuration={timeDuration}
+            onFinish={onFinish}
+          />
+        </Box>
       </Box>
     </Box>
   );
