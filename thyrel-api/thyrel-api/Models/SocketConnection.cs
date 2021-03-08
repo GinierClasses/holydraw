6a99949ac5ae9c7b27290a8441ea3226c0281576
Melvynx <melvyn.malherbe@qoqa.com>
2021-03-08 08:20:55 +0100

    fix: remove warning

---
+++ thyrel-api/thyrel-api/Models/SocketConnection.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Net.WebSockets;
+
+namespace thyrel_api.Websocket
+{
+    public class SocketConnection
+    {
+        public Guid Id { get; set; }
+        public WebSocket WebSocket { get; set; }
+        public int? RoomId { get; set; }
+        public int? PlayerId { get; set; }
+    }
+}
