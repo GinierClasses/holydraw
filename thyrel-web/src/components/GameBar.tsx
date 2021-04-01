deb43bf3f888ab3dfc9bff0b4437362724ef304d
Melvynx <melvyn.malherbe@qoqa.com>
2021-04-01 15:35:51 +0200

    ref: upgrade game bar

--- thyrel-web/src/components/GameBar.tsx
+++ thyrel-web/src/components/GameBar.tsx
@@ -2,46 +2,37 @@ import PlayerCount from './room/PlayerCount';
 import AppTitle from './AppTitle';
 import StepTimer from './room/StepTimer';
 import { useSessionContext } from 'hooks/SessionProvider';
-import { Box, makeStyles } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 
 type GameBarProps = {
   onFinish?: () => void;
 };
 
-const useStyles = makeStyles(theme => ({
-  infoContainer: {
-    [theme.breakpoints.up('sm')]: {
-      top: -64,
-      position: 'relative',
-    },
-  },
-}));
-
 export default function GameBar({ onFinish }: GameBarProps) {
   const { session } = useSessionContext();
-  const classes = useStyles();
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" width="100%">
-      <AppTitle />
-
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        width="100%"
-        className={classes.infoContainer}>
+    <Box
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+      width="100%">
+      <Box order={{ xs: 2, sm: 1 }}>
         <PlayerCount
           count={session?.playerFinished || 0}
           max={session?.totalPlayers || 0}
         />
-
-        <Box display="block">
-          <StepTimer
-            finishAt={new Date(session?.stepFinishAt || '')}
-            timeDuration={session?.timeDuration || 60}
-            onFinish={onFinish}
-          />
-        </Box>
+      </Box>
+      <Box width={{ xs: '100%', sm: 'auto' }} order={{ xs: 1, sm: 2 }}>
+        <AppTitle />
+      </Box>
+      <Box order={3}>
+        <StepTimer
+          finishAt={new Date(session?.stepFinishAt || '')}
+          timeDuration={session?.timeDuration || 60}
+          onFinish={onFinish}
+        />
       </Box>
     </Box>
   );
