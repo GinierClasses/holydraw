5f78ae90a857de1299e79ffaf767e2376221cd70
Villou <dkulic01@gmail.com>
2021-04-08 12:21:19 +0200

    add: test avec l'image gymguy

--- thyrel-web/src/pages/ComponentTest.tsx
+++ thyrel-web/src/pages/ComponentTest.tsx
@@ -22,6 +22,7 @@ import SpinnerIcon from 'components/SpinnerIcon';
 import StartButton from 'components/room/lobby/StartButton';
 import StepProgress from 'components/room/StepProgress';
 import { Slider } from '@material-ui/core';
+import GymGuy from 'images/gym-guy.svg';
 import BookDrawingElement from 'components/room/book/BookDrawingElement';
 
 const colors = [
@@ -83,7 +84,7 @@ export default function ComponentTest() {
           finishAt={new Date('2021-03-30T08:31:00')}
           timeDuration={120}
         />
-        <BookDrawingElement username="Jean-Philippes-Pascal" />
+        <BookDrawingElement username="Jean-Philippes-Pascal" src={GymGuy} />
         <ButtonModalJoin
           loading={false}
           identifier={undefined}
