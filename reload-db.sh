36b47ca341af3c63753c6764e94a78c9fd69076a
TacticsCH <alexandre.javet@hotmail.com>
2021-04-19 14:41:56 +0200

    feat: Update reload-db
    - Yes is now a default answer to the prompt, which means you can press enter and it will consider it as a yes
    - Added logo

--- reload-db.sh
+++ reload-db.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env bash
+
+# set -e -x
+
 # Thyrel team - database reconstructor/reloader script
 # this script will remove the projects images and rebuild them
 # USE THIS SCRIPT EACH TIME YOU MODIFY THE DATABASE'S STRUCTURE
 # ---
-# Version 1 - 27.01.2021
+# Version 1.1 - 27.01.2021
 # --------------------------------------------------------------
 # Notice : Please execute it in the project's folder otherwise it won't work
 # since it uses docker commands that will be executed locally.
@@ -16,20 +20,29 @@ fi
 
 if ! [[ "$(command -v docker-compose)" ]]; then
    echo -e "\e[31mWARNING:\e[39m docker-compose is not installed";
-   echo -e "\e[34mCOMMAND:\e[39m please install docker composeß";
+   echo -e "\e[34mCOMMAND:\e[39m please install docker compose";
    exit 1;
 fi
 
+ echo -e "--------------------------------------"
+ echo -e " _  _     _      ___                  "
+ echo -e "| || |___| |_  _|   \ _ _ __ ___ __ __"
+ echo -e "| __ / _ \ | || | |) | '_/ _\` \ V  V /"
+ echo -e "|_||_\___/_|\_, |___/|_| \__,_|\_/\_/ "
+ echo -e "            |__/                      "
+ echo -e "                             DATABASE RELOADER V1.1"
+ echo -e "--------------------------------------"
+ echo
+
 read -p "Are you sure to do that? your images will be redownloaded. [Y/n]" -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ ! $REPLY =~ ^[Yy]|$ ]]
 then
     exit 1
 fi
 
 docker container rm dev_mysql
 docker container rm dev_pma
-
 docker-compose up 
 
 echo
