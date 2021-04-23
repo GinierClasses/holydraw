b0000017bd8e3875425984ca02c7988b5a377cef
Melvynx <melvyn.malherbe@qoqa.com>
2021-04-23 11:21:53 +0200

    add: next album controller

--- thyrel-web/src/pages/room/Book.tsx
+++ thyrel-web/src/pages/room/Book.tsx
@@ -4,26 +4,29 @@ import GameLayout from 'components/room/GameLayout';
 import { Grid } from '@material-ui/core';
 import BookStartAction from 'components/room/book/BookStartAction';
 import { useRoomContext } from 'hooks/RoomProvider';
+import { AlbumContextProvider } from 'hooks/AlbumProvider';
 
 export default function Book() {
   const { players } = useRoomContext();
   return (
-    <GameLayout displayHud={false} maxWidth="sm">
-      <Grid
-        container
-        spacing={2}
-        direction="column"
-        alignItems="center"
-        className="full-height"
-        wrap="nowrap"
-        justify="space-between">
-        <Grid item>
-          <BookPlayerList players={players} />
+    <AlbumContextProvider>
+      <GameLayout displayHud={false} maxWidth="sm">
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          alignItems="center"
+          className="full-height"
+          wrap="nowrap"
+          justify="space-between">
+          <Grid item>
+            <BookPlayerList players={players} />
+          </Grid>
+          <Grid item>
+            <BookStartAction />
+          </Grid>
         </Grid>
-        <Grid item>
-          <BookStartAction />
-        </Grid>
-      </Grid>
-    </GameLayout>
+      </GameLayout>
+    </AlbumContextProvider>
   );
 }
