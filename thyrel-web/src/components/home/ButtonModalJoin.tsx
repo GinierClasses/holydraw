473864160d23adb7e5e34b5c4feb0cf150018e55
Melvynx <melvyn.malherbe@qoqa.com>
2021-04-28 09:24:45 +0200

    fix: color of TextField on ButtonModalJoin rename ModalJoin to ButtonModalJoin

--- thyrel-web/src/components/home/ButtonModalJoin.tsx
+++ thyrel-web/src/components/home/ButtonModalJoin.tsx
@@ -10,19 +10,19 @@ import {
   TextField,
 } from '@material-ui/core';
 
-type ModalJoinProps = {
+type ButtonModalJoinProps = {
   identifier?: string;
   onClick: (identifier: string) => void;
   loading?: boolean;
   className?: string;
 };
 
-export default function ModalJoin({
+export default function ButtonModalJoin({
   identifier: urlIdentifier,
   loading,
   onClick,
   className,
-}: ModalJoinProps) {
+}: ButtonModalJoinProps) {
   const [open, setOpen] = React.useState(false);
   const [identifier, setIdentifier] = React.useState('');
   return (
@@ -46,6 +46,7 @@ export default function ModalJoin({
         <DialogTitle>Join a game 👨‍🎨</DialogTitle>
         <DialogContent>
           <TextField
+            color="secondary"
             autoFocus
             margin="dense"
             id="name"
