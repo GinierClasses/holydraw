c252b7c7cc434d22f8fdee60de1e68cbdcb9ef76
Melvynx <melvyn.malherbe@qoqa.com>
2021-05-03 20:49:08 +0200

    add: vertical sizepicker

--- thyrel-web/src/components/room/draw/SizePickerV2.tsx
+++ thyrel-web/src/components/room/draw/SizePickerV2.tsx
@@ -1,15 +1,18 @@
-import { Box, Slider } from '@material-ui/core';
+import { Box, Slider, Tooltip } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 type SizePickerV2Props = {
   defaultSize: number;
   onSizeChange?: (event: any, newValue: any) => void;
+  orientation?: 'vertical' | 'horizontal';
 };
 
 const SizeSlider = withStyles({
   root: {
     color: '#C6C6C6',
     borderRadius: 4,
+    height: 8,
+    width: 300,
   },
   thumb: {
     height: 16,
@@ -29,31 +32,79 @@ const SizeSlider = withStyles({
     height: 8,
     borderRadius: 4,
   },
+  vertical: {
+    // important because width is overide by default vertical class
+    width: '8px !important',
+    '& .$MuiSlider-rail': {
+      width: 8,
+    },
+    '& .$MuiSlider-track': {
+      width: 8,
+    },
+    '& .$MuiSlider-thumb': {
+      marginLeft: '-4.15px',
+    },
+  },
 })(Slider);
+
+interface Props {
+  children: React.ReactElement;
+  open: boolean;
+  value: number;
+}
+
+function ValueLabelComponent(props: Props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
 
 export default function SizePickerV2({
   defaultSize,
   onSizeChange,
+  orientation = 'vertical',
 }: SizePickerV2Props) {
+  const isVertical = orientation === 'vertical';
   return (
     <Box
-      width={377}
-      height={46}
+      p={2}
       bgcolor="#272B31"
       borderRadius={40}
       display="flex"
-      flexDirection="row"
-      justifyContent="space-around"
+      flexDirection={isVertical ? 'column' : 'row'}
+      justifyContent={isVertical ? 'center' : 'space-around'}
       alignItems="center">
-      <Box bgcolor="#C6C6C6" borderRadius="50%" height={8} width={8} />
-      <Box width="300px">
-        <SizeSlider
-          aria-labelledby="continuous-slider"
-          defaultValue={defaultSize}
-          onChange={onSizeChange}
-        />
-      </Box>
-      <Box bgcolor="#C6C6C6" borderRadius="50%" height={24} width={24} />
+      <Box
+        bgcolor="#C6C6C6"
+        mr={isVertical ? 0 : 2}
+        mb={isVertical ? 2 : 0}
+        borderRadius="50%"
+        height={8}
+        width={8}
+      />
+      <SizeSlider
+        step={2}
+        min={2}
+        style={isVertical ? { height: 300 } : { width: 300 }}
+        max={60}
+        aria-labelledby="size-picker-slider"
+        orientation={orientation}
+        defaultValue={defaultSize}
+        onChange={onSizeChange}
+        ValueLabelComponent={ValueLabelComponent}
+      />
+      <Box
+        bgcolor="#C6C6C6"
+        ml={isVertical ? 0 : 2}
+        mt={isVertical ? 2 : 0}
+        borderRadius="50%"
+        height={24}
+        width={24}
+      />
     </Box>
   );
 }
