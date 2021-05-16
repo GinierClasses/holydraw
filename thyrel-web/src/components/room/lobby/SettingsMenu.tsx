456769ce0b1b8f47f89ccbb321a22f52030daa5e
Villou <dkulic01@gmail.com>
2021-05-16 14:01:38 +0200

    fix: player

--- thyrel-web/src/components/room/lobby/SettingsMenu.tsx
+++ thyrel-web/src/components/room/lobby/SettingsMenu.tsx
@@ -17,7 +17,7 @@ export default function SettingsMenu() {
       flexDirection="column"
       alignItems={{ xs: 'center', sm: 'flex-start' }}
       gridGap={24}>
-      <ShareRoomButton identifier={room?.identifier} />
+      <ShareRoomButton identifier={room?.identifier} player={player} />
       <Typography variant="body1">Debug: {wsState}</Typography>
     </Box>
   );
