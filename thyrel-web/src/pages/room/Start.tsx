991d790cd773fea52bfdf2dd1c5e880db5e1d6cc
Villou <dkulic01@gmail.com>
2021-05-17 11:02:06 +0200

    add: adapt "start a story"

--- thyrel-web/src/pages/room/Start.tsx
+++ thyrel-web/src/pages/room/Start.tsx
@@ -2,8 +2,11 @@ import GymGuy from 'images/gym-guy.svg';
 import { Grid, Typography } from '@material-ui/core';
 import SentenceForm from 'components/room/start/SentenceForm';
 import GameLayout from 'components/room/GameLayout';
+import { useRoomContext } from 'hooks/RoomProvider';
+import { RoomMode } from 'types/Room.type';
 
 export default function Start() {
+  const { room } = useRoomContext();
   return (
     <GameLayout maxWidth="sm">
       <Grid
@@ -18,7 +21,11 @@ export default function Start() {
           <img src={GymGuy} alt="" width={256} />
         </Grid>
         <Grid item>
-          <Typography variant="h4">Start a story</Typography>
+          <Typography variant="h4">
+            {room?.roomMode === RoomMode.Standard
+              ? 'Start a story'
+              : 'Choose a word'}
+          </Typography>
         </Grid>
         <SentenceForm />
       </Grid>
