5d46837f03c85c6cf5e28fcd9d7bf0da5c982294
Villou <dkulic01@gmail.com>
2021-05-28 15:26:35 +0200

    fix: improve modal component

--- thyrel-web/src/components/room/lobby/ModeModal.tsx
+++ thyrel-web/src/components/room/lobby/ModeModal.tsx
@@ -2,11 +2,19 @@ import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-import { Box, Typography } from '@material-ui/core';
+import { Box, DialogTitle } from '@material-ui/core';
+import { RoomMode } from 'types/Room.type';
+import ModeCard from './ModeCard';
 
 type ModeModalProps = {
   open: boolean;
   onClose: () => void;
+  onSelect: (mode: RoomMode) => void;
+};
+
+type InfoModeProps = {
+  title: string;
+  description: string;
 };
 
 const useStyles = makeStyles(theme => ({
@@ -18,7 +26,18 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function ModeModal({ open, onClose }: ModeModalProps) {
+const infoMode: Record<RoomMode, InfoModeProps> = {
+  [RoomMode.Standard]: {
+    title: 'Standard',
+    description: 'A simple mode where you draw and guess.',
+  },
+  [RoomMode.OneWord]: {
+    title: 'OneWord',
+    description: 'As Standard Mode but with only one word.',
+  },
+};
+
+export default function ModeModal({ open, onClose, onSelect }: ModeModalProps) {
   const classes = useStyles();
 
   return (
@@ -26,16 +45,26 @@ export default function ModeModal({ open, onClose }: ModeModalProps) {
       <Box
         display="flex"
         flexDirection="row"
-        maxWidth="100%"
-        justifyContent="center"
         bgcolor="background.paper"
-        width={572}
+        width={500}
         height={312}>
-        <Box mt={2}>
-          <Typography variant="h4">Settings</Typography>
-          <IconButton className={classes.closeButton} onClick={() => onClose()}>
-            <CloseIcon />
-          </IconButton>
+        <DialogTitle>Settings</DialogTitle>
+        <IconButton className={classes.closeButton} onClick={() => onClose()}>
+          <CloseIcon />
+        </IconButton>
+        <Box
+          display="flex"
+          flexDirection="row"
+          flexWrap="wrap"
+          justifyContent="center"
+          mt={12}>
+          {Object.keys(infoMode).map((key: string) => {
+            const roomMode = Number(key) as RoomMode;
+            const mode = infoMode[roomMode];
+            return (
+              <ModeCard {...mode} onClick={() => onSelect(roomMode)}></ModeCard>
+            );
+          })}
         </Box>
       </Box>
     </Dialog>
