7a7e39c61e332cbfa27192f2b4d0e38057fb61b4
Melvynx <melvyn.malherbe@qoqa.com>
2021-06-05 17:40:14 +0200

    :lipstick: add ws debug panel in absolute alert

--- thyrel-web/src/components/room/lobby/SettingsMenu.tsx
+++ thyrel-web/src/components/room/lobby/SettingsMenu.tsx
@@ -1,11 +1,11 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import { usePlayerContext } from 'hooks/PlayerProvider';
 import { useRoomContext } from 'hooks/RoomProvider';
 import RoomModeSelector from './RoomModeSelector';
 import ShareRoomButton from './ShareRoomButton';
 
 export default function SettingsMenu() {
-  const { room, wsState } = useRoomContext();
+  const { room } = useRoomContext();
   const { player } = usePlayerContext();
 
   return (
@@ -19,7 +19,6 @@ export default function SettingsMenu() {
       alignItems={{ xs: 'center', sm: 'flex-start' }}
       gridGap={24}>
       <ShareRoomButton identifier={room?.identifier} player={player} />
-      <Typography variant="body1">Debug: {wsState}</Typography>
       <RoomModeSelector />
     </Box>
   );
