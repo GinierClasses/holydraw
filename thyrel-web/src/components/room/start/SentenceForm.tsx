af4f9901e7009ef2087dc6955dbf9ea7f8fdc151
Villou <dkulic01@gmail.com>
2021-06-07 14:11:11 +0200

    fix: allow player to save the default sentence

--- thyrel-web/src/components/room/start/SentenceForm.tsx
+++ thyrel-web/src/components/room/start/SentenceForm.tsx
@@ -45,7 +45,7 @@ export default function StartForm() {
   function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
     if (loading || e.key !== 'Enter') return;
     setLoading(true);
-    onSave(sentence).then(() => setLoading(false));
+    onSave(sentence || defaultSentence).then(() => setLoading(false));
   }
 
   useTimerEvent({
@@ -86,10 +86,9 @@ export default function StartForm() {
             className={classes.button}
             color="primary"
             loading={loading}
-            disabled={sentence.length === 0}
             onClick={() => {
               setLoading(true);
-              onSave(sentence).then(() => setLoading(false));
+              onSave(sentence || defaultSentence).then(() => setLoading(false));
             }}
             startIcon={
               isEditing ? (
