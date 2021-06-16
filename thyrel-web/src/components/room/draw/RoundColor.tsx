c06c7caa9d9cb3ee7f35c468a908f2e1cca1d176
lulu <lucamaggio98@hotmail.com>
2021-06-16 20:01:24 +0200

    add: add RoundColor component

---
+++ thyrel-web/src/components/room/draw/RoundColor.tsx
@@ -0,0 +1,41 @@
+import { SvgIconTypeMap } from '@material-ui/core';
+import { Box, useTheme } from '@material-ui/core';
+import { OverridableComponent } from '@material-ui/core/OverridableComponent';
+
+type DrawColorPickerProps = {
+  color: string;
+  isSelected: boolean;
+  colorWithDisabled: string;
+  onClick: () => void;
+  Icon?: OverridableComponent<SvgIconTypeMap<{}, 'svg'>>;
+  width?: number;
+};
+
+export default function DesktopColorPicker({
+  color,
+  isSelected,
+  colorWithDisabled,
+  onClick,
+  Icon,
+  width,
+}: DrawColorPickerProps) {
+  width ? (width = width) : (width = 42);
+  return (
+    <Box
+      component="button"
+      key={color}
+      border={2}
+      m={0.5}
+      p={0}
+      bgcolor={colorWithDisabled}
+      boxShadow={isSelected ? 4 : 0}
+      borderColor={isSelected ? '#ffffff' : colorWithDisabled}
+      width={width}
+      height={42}
+      borderRadius={width ? 21 : '50%'}
+      className="cursor-pointer"
+      onClick={onClick}>
+      {Icon ? <Icon /> : null}
+    </Box>
+  );
+}
